0fbdb7b415ed0e461d8831bdc97462fa11d15254
hidraulica2000 <hidraulica2000@gmail.com>
2013-03-01 21:09:53 -0500

    5th try on active admin heroku issue fix
    Signed-off-by: hidraulica2000 <hidraulica2000@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module Tengops
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js']
+    config.assets.precompile += ['active_admin.css', 'active_admin.js']
   end
 end
