16935c0b5f59c16416489a336cd9418ba92b24dd
tomasherrera <tomas.herrera@koombea.com>
2013-03-14 16:02:08 -0500

    Game Model's attributes set to accesible in order to be able to modify them in the future from active admin
    Signed-off-by: tomasherrera <tomas.herrera@koombea.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :game_title, :platform_id, :release_date, :overview, :esrb_id, :genre_id, :players, :coop, :youtube, :publisher_id, :developer_id, :boxart, :boxart_thumb, :boxart_normal
   belongs_to :esrb
   belongs_to :genre
   belongs_to :platform
